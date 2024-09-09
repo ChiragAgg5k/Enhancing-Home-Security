fd9d86096c683a0ce4f9023951b5269748d7555b
ChiragAgg5k <chiragaggarwal5k@gmail.com>
2024-09-09 10:01:52 +0530

    fixed some stuff

--- app.py
+++ app.py
@@ -3,15 +3,11 @@ from flask import Flask, render_template, Response
 import cv2
 from ultralytics import YOLO
 
-classNames = [
-    "gun",
-    "Knife"
-]
+classNames = ["ammo", "firearm", "grenade", "knife", "pistol", "rocket"]
 
 app = Flask(__name__)
 camera = cv2.VideoCapture(0)
 
-# model = YOLO("yolov8n.pt")
 model = YOLO("YOLO_ThreatDetection_v1.0.pt")
 
 def gen_frames():
@@ -47,23 +43,20 @@ def gen_frames():
                 color = (255, 0, 0)
                 thickness = 2
 
-                cv2.putText(
-                    frame, classname, org, font, fontScale, color, thickness
-                )
+                cv2.putText(frame, classname, org, font, fontScale, color, thickness)
 
         if not success:
             break
         else:
             ret, buffer = cv2.imencode(".jpg", frame)
             frame = buffer.tobytes()
-            yield (
-                b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n"
-            ) 
+            yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n")
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.route("/video_feed")
 def video_feed():
